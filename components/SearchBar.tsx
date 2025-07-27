4c75ed38c2172c08c160335c8f33235166c3251d
krrakshit <rkrkr365@gmail.com>
2025-07-27 14:43:44 +0530

    Remove unused useRouter import from SearchBar component and added infinite scrolling feature

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useMemo } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { Company } from '@/lib/types';
 
 interface SearchBarProps {
